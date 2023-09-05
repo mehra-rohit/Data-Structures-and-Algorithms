32608fb55d5fec3cd1b3c671b117e33789c74a75
Rohit Mehra <93056883+mehra-rohit@users.noreply.github.com>
2023-09-05 23:47:51 +0530

    binary_search.py

--- Algorithms/Binary Search/temp.py
+++ Algorithms/Binary Search/temp.py
@@ -1,1 +1,17 @@
-# Temp file
+# Binary Search Implementation
+
+def binaray_search(arr, target):
+
+  start = 0
+  end = len(arr) - 1
+  
+  while start <= end:
+    mid = (start + end)//2
+    if arr[mid]==target:
+      return mid,arr
+    elif arr[mid]<target:
+      start = mid + 1
+    else :
+      end = mid-1
+      
+  return f'{target} not present in array'
