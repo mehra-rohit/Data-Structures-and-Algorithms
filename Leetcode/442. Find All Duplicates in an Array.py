ddd694d10b637c9cb4b98126a21096b561bf0692
Rohit Mehra <93056883+mehra-rohit@users.noreply.github.com>
2023-09-09 02:06:14 +0530

    Create 442. Find All Duplicates in an Array.py

---
+++ Leetcode/442. Find All Duplicates in an Array.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def swap_elements(self, nums, a, b):
+        temp = nums[a]
+        nums[a]=nums[b]
+        nums[b]=temp
+        return nums
+
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        i = 0
+        while i<len(nums):
+            correct = nums[i]-1
+            if nums[i]!=nums[correct]:
+                self.swap_elements(nums,i,correct)
+            else:
+                i += 1
+        
+        for i in range(len(nums)):
+            if i+1 != nums[i]:
+                ans.append(nums[i])
+        return ans
