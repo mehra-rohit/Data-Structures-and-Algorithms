89f0a552186def57d668a36e459f0443c8eb2ae8
Rohit Mehra <93056883+mehra-rohit@users.noreply.github.com>
2023-09-09 23:18:06 +0530

    added multiple occurence case

--- Algorithms/Recursion/linear-search.py
+++ Algorithms/Recursion/linear-search.py
@@ -11,3 +11,16 @@ def linear_search(arr,target, i = 0):
 
     
 print(linear_search([1,2,3,1,1,2,3],10))
+
+#multiple occurences
+def linear_search(arr,target, i = 0,ind = []):
+    
+    #base condition
+    if i<len(arr):
+        if arr[i]==target:
+            ind.append(i)
+        return linear_search(arr,target,i+1,ind)
+    else:
+        return ind
+    
+print(linear_search([1,2,3,10,10,2,3,10],10))
