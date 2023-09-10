4ae8ef4b89fbf403b7b2c11f5279faaf248785ee
Rohit Mehra <93056883+mehra-rohit@users.noreply.github.com>
2023-09-10 14:49:49 +0530

    Update remove-char-from-string.py

--- Algorithms/Recursion/remove-char-from-string.py
+++ Algorithms/Recursion/remove-char-from-string.py
@@ -1,5 +1,5 @@
 #remove all a's from string
-
+#recursive way
 def string_func(string,i=0,ans=''):
     if i==len(string):
         return ans
@@ -8,6 +8,19 @@ def string_func(string,i=0,ans=''):
     else : 
         ans = string[i]
     return ans + string_func(string,i+1)
+        
+
+print(string_func('baccadsafasfasfas'))
+
+#with loop
+#remove all a's from string
+def string_func(string):
+    ans = ''
+    for i in string:
+        if i=='a':
+            pass
+        else : ans += i
+    return ans
         
 
 print(string_func('baccadsafasfasfas'))
